1a66eef01c7c4a446b48c90cf269e5573ea4e156
eliasalvaradocr <eliasalvaradocr@gmail.com>
2023-09-02 19:33:59 -0600

    Se modifica el diseño hardware, mas robusto. Con el software se crea la maquina de estados base

--- Laboratorio2/semaforo/test.c
+++ Laboratorio2/semaforo/test.c
@@ -3,31 +3,101 @@
 #include <stdbool.h>
 
 // Definicion de estados por utilizar
-#define estado_inicio 0
-#define transicion    1
-#define peatones      2
-#define vehiculos     3
+
+#define estado_inicio 1 // Estado inicial de la maquina de estados
+#define transicion    2 // Estado de transicion
+#define peatones      3 // Estado donde tiene el paso de peatones
+#define vehiculos     4 // Estado para el paso de vehiculos
 
 // Se declaran las variables
-int actual_me;
-int next_me;
-
+int actual_me;          // Estado actual de la me
+int next_me;            // Proximo estado de la me
+int out;                // Variable para controlar la salida
+bool change_state= false; // Variable que indica si se debe de cambiar el estado
 
 // Declaración de funciones por utilizar
-void me();
-void delay();
+void me();              // Funcion donde se establece la maquina de estados por manejar
+void delay();           // Funcion que introduce retardos
 
 // Funcion main
 int main(void){
+    // Se realiza una inicializacion de estados
+    next_me   = estado_inicio; //Establece el estado de inicio
+    actual_me = next_me;
+    
+    //Se configuran los registros del microcontrolador
+    // Se inicializan en cero los siguientes:
+    PORTB = 0x00; 
+    PIND = 0X00;
+    PORTD = 0x00;
+    
+    // Se presenta la configuración del puerto B como salidas
+    DDRB = 0b01111111;
 
+    // Se configuran los pines de GPIOS dos y tres como entradas
+    DDRD = 0b11110011;  
+ 
+    // Configuración de las interrupciones:
+    // Se habilitan las interrupciones INT0 y INT1
+    GIMSK |= (1<<INT0);  
+    GIMSK |= (1<<INT1);
+    // Configuración de los flancos de disparo para las interrupciones
+    MCUCR |= (1<<ISC00)|(1<<ISC01);     
+
+    // Llamar a la función que maneja la máquina de estados
+    while(1){
+        me();
+    }
+
+    // Habilitar interrupciones globales
+    sei();
+
+    return 0;
 }
-
-// En la siguiente funcion se busca implementar la máquina de estados
+ 
 void me(){
+    // En esta seccion se usa un switch para considerar cada caso de la me
+   switch(actual_me){ 
+
+    case estado_inicio:
+        out = 0b00000000;    // Configuración de la salida
+        PORTB = out;        // Se establece la salida en el puerto B
+        next_me = vehiculos;// Cambio al estado de permitir vehículos
+
+        break;
+
+    case transicion:
+        out = 0b00101001;   // Configuración de la salida
+        PORTB = out;        // Configuración de la salida
+        
+        break;  
+
+    case peatones:
+        out = 0b00101001;   // Configuración de la salida
+        PORTB = out;        // Se establece la salida en el puerto B 
+        
+        break;
+
+    case vehiculos: 
+        if (change_state) // Si se debe cambiar de estado
+        {
+            next_me = transicion; // Cambio al estado de transición
+            out = 0b00101001;     // Configuración de la salida
+            PORTB = out;          // Se establece la salida en el puerto B
+            break;
+        }
+        else{
+            next_me = transicion;   // Cambio al estado de transición
+            out = 0b00101001;       // Configuración de la salida
+      break; 
+      }
+  }
 
 }
+// Las rutinas de interrupcion:
 
-// Funcion por implementar delay
-void delay(){
-
-}
+// Actualización del estado actual con el próximo estado
+ISR (INT0_vect){actual_me = next_me;}
+// Actualización del estado actual con el próximo estado
+ISR (INT1_vect){actual_me = next_me;}
+ 
