aff2523a30bbe0f241ae42affdeed812367d619e
eliasalvaradocr <eliasalvaradocr@gmail.com>
2023-09-06 16:02:32 -0600

    Comentarios
     Se añaden comentarios al codigo

--- Laboratorio2/src/semaforo.c
+++ Laboratorio2/src/semaforo.c
@@ -1,13 +1,28 @@
+/**
+ * @file semaforo.c
+ * @authors 
+ *      - Sofia Villalta Jinesta
+ *      - Elias Alvarado Vargas
+ * @brief Programa para controlar un semáforo en un microcontrolador ATtiny4313.
+ * @version 0.1
+ * @date 2023-09-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
-#define estado_inicio 1
-#define parpadeo_amarillo 2
+// Definiciones de estados
+#define estado_inicio           1
+#define parpadeo_amarillo       2
 #define rojo_semaforo_vehicular 3
 #define verde_semaforo_peatonal 4
 #define parpadeo_peatonal       5
 
+// Variables Globales
 int actual_me;
 int next_me;
 bool button_pressed = false;
@@ -17,13 +32,20 @@ volatile int button_counter = 0;
 volatile bool button_triggered = false;
 volatile int blink_counter = 0;
 
-
+// Prototipo de funcion
 void me();
+
+/**
+ * @brief Función principal del programa.
+ * 
+ * @return int Valor de retorno del programa.
+ */
 
 int main(void) {
     next_me = estado_inicio;
     actual_me = next_me;
 
+    // Configuración de puertos y pines
     PORTB = 0b00000000;
     PIND = 0b00000000;
     PORTD = 0b00000000;
@@ -31,8 +53,9 @@ int main(void) {
     DDRB = 0b01111111;
     DDRD = 0b11110011;
 
-    GIMSK |= (1 << INT0); //botón 1
-    GIMSK |= (1 << INT1);  // botón 2
+    // Configuración de interrupciones externas para botones
+    GIMSK |= (1 << INT0); // Botón 1
+    GIMSK |= (1 << INT1);  // Botón 2
     MCUCR |= (1 << ISC00) | (1 << ISC01);
     MCUCR |= (1 << ISC10) | (1 << ISC11);  // Configurar INT1 para flanco de subida
 
@@ -41,7 +64,7 @@ int main(void) {
     TCCR0B = (1 << CS02) | (1 << CS00); // Preescalador 
     TIMSK |= (1 << TOIE0); // Habilitar interrupción de desbordamiento
 
-    sei();
+    sei(); // Habilitar interrupciones globales
 
     while (1) {
         me();
@@ -50,13 +73,17 @@ int main(void) {
     return 0;
 }
 
+/**
+ * @brief Función para manejar la lógica de la máquina de estados.
+ * 
+ */
 
 
 void me() {
     switch (actual_me) {
 
     case estado_inicio:
-        PORTB = 0b00101001;  // estado inicial
+        PORTB = 0b00101001;  // Estado inicial
         if (delay_counter >= 680 && button_triggered) { //10 segundos
             PORTB &= ~(1 << PB0);  // Asegurarse que B0 esté apagado
             next_me = parpadeo_amarillo;
@@ -66,7 +93,7 @@ void me() {
         break;
 
     case parpadeo_amarillo:
-        if (delay_counter < 204) { // después de 3s
+        if (delay_counter < 204) { // Después de 3 segundos
             // Parpadea cada segundo para que sea visible
             if (blink_counter % 2 == 0) {
                 PORTB |= (1 << PB1);  // Encender LED B1
@@ -109,10 +136,8 @@ void me() {
             }
         break;
         
-        
-
     case parpadeo_peatonal:
-    if (delay_counter < 204) { // después de 3s
+    if (delay_counter < 204) { // Después de 3s
         // Parpadea cada segundo para que sea visible
         if (blink_counter % 2 == 0) {
             PORTB |= (1 << PB6) | (1 << PB4);  // Encender LED B6 y B4
@@ -129,16 +154,14 @@ void me() {
         next_me = estado_inicio;
         delay_counter = 0;  // Resetear el contador
     }
-    break;
-
-    
+    break;    
 }
 actual_me = next_me;
 
 }
 
 
-
+// Manejadores de interrupcion
 ISR(INT0_vect) {
     button_triggered = true;
 }
