c3c15ecf953dc997c2dbd051ec7533ae00c00312
eliasalvaradocr <eliasalvaradocr@gmail.com>
2023-10-17 22:56:48 -0600

    Modificacion
    Inicio labo4, se agrega base para pantalla, makefiles, y otro base de archivo sismografo con ejes

---
+++ Laboratorio4/sismografo.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdio.h>
+
+// Includes de lcd-serial
+#include "lcd-spi.h"
+#include "clock.h"
+#include "console.h"
+#include "sdram.h"
+#include "gfx.h"
+
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+
+int main(void){
+	// Creacion de struct para giroscopios
+	int16_t x = 0;
+	int16_t y = 0;
+	int16_t z = 0;
+}
