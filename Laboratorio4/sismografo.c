306e45af03c3198db4d1552da2d631d26323b291
Usuario <sofia.villaltajinesta@ucr.ac.cr>
2023-10-22 17:41:05 -0600

    IOT y UART

--- Laboratorio4/sismografo.c
+++ Laboratorio4/sismografo.c
@@ -27,10 +27,16 @@
 
 float battery_voltage;
 
-void gpio_setup_for_led(void) {
+void gpio_setup_for_ledR(void) {
     rcc_periph_clock_enable(RCC_GPIOG);
     gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO14);
 }
+
+void gpio_setup_for_ledG(void) {
+    rcc_periph_clock_enable(RCC_GPIOG);
+    gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+}
+
 
 static uint16_t read_adc_naiive(uint8_t channel) {
     uint8_t channel_array[16];
@@ -49,6 +55,38 @@ uint16_t read_adc_average(uint8_t channel) {
     return sum / NUM_SAMPLES;
 }
 
+static void usart_setup(void)
+{
+	/* Setup GPIO pins for USART1 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+
+	/* Setup USART1 TX pin as alternate function. */
+	
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
+
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+    gpio_set(GPIOG, GPIO13);
+}
+
+void usart_send_string(char* str)
+{
+    while(*str)
+    {
+        usart_send_blocking(USART1, *str++);
+        gpio_toggle(GPIOG, GPIO13);  // Hace que el LED parpadee
+            for (int i = 0; i < LED_BLINK_INTERVAL; i++) {
+                __asm__("nop");
+        }
+    }
+}
+
 float adc_to_voltage(uint16_t adc_value) {
     float voltage = (adc_value / ADC_MAX_VALUE) * VREF;
     return voltage * CONVERSION_FACTOR;
@@ -56,7 +94,7 @@ float adc_to_voltage(uint16_t adc_value) {
 
 void adc_setup(void) {
     rcc_periph_clock_enable(RCC_ADC1);
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);  // Cambiado a GPIO0
+    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO7);  
     adc_power_off(ADC1);
     adc_enable_scan_mode(ADC1);
     adc_set_continuous_conversion_mode(ADC1);
@@ -66,8 +104,18 @@ void adc_setup(void) {
     adc_power_on(ADC1);
 }
 
+void button_setup(void) {
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0); 
+}
+bool is_button_pressed(void) {
+    return gpio_get(GPIOA, GPIO0);
+    }
+
+
 int main(void) {
     char lcd_out[100];
+    char usart_out[200]; 
     int16_t x, y, z;
 
     clock_setup();
@@ -77,7 +125,9 @@ int main(void) {
     adc_setup();
     lcd_spi_init();
     gfx_init(lcd_draw_pixel, 240, 320);
-    gpio_setup_for_led();
+    gpio_setup_for_ledR();
+    gpio_setup_for_ledG();
+    usart_setup();
 
     while (1) {
         gfx_fillScreen(LCD_WHITE);
@@ -99,7 +149,7 @@ int main(void) {
         gfx_setCursor(15, 144);
         gfx_puts(lcd_out);
 
-        uint16_t raw_adc_value = read_adc_average(0); // Canal 0
+        uint16_t raw_adc_value = read_adc_average(7); // Canal 7
         battery_voltage = adc_to_voltage(raw_adc_value);
 
         if (battery_voltage < 7.0f) {
@@ -118,6 +168,11 @@ int main(void) {
         sprintf(lcd_out, "Voltaje: %.2fV", battery_voltage);
         gfx_setCursor(15, 250);
         gfx_puts(lcd_out);
+        
+        if (is_button_pressed()) {
+        sprintf(usart_out, "%.2f\t%.2f\t%.2f\t%.2f\r\n", (float)x/100, (float)y/100, (float)z/100, battery_voltage);
+        usart_send_string(usart_out);  
+        }
 
         lcd_show_frame();
 
