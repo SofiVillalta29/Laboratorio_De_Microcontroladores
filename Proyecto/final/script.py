b1eb035754de10d4518f22482c36b0daea089ace
Usuario <sofia.villaltajinesta@ucr.ac.cr>
2023-12-02 14:15:40 -0600

    proyectofinalMicros

--- Proyecto/final/script.py
+++ Proyecto/final/script.py
@@ -2,28 +2,44 @@ import serial
 import re
 import sys
 import select
+import paho.mqtt.client as mqtt
+import json
 
 # Inicia conexión serial
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
+# Configuración MQTT para Thingsboard
+broker = "iot.eie.ucr.ac.cr"
+port = 1883
+token = "n4j4928th9gtlnnhngee"  # Tu token real
+device_id = "31db47c0-8728-11ee-9e77-a52136d4a347"  # Tu Device ID
+
+client = mqtt.Client()
+client.username_pw_set(token)
+client.connect(broker, port, 60)
+client.loop_start()
+
+def on_publish(client, userdata, result):
+    print("Data published to Thingsboard")
+    pass
+
+client.on_publish = on_publish
+
 # Expresión regular para capturar las etiquetas y valores
 pattern = re.compile(r"(\w+): (\d+\.\d+)")
 
-# Variables globales para almacenar la última nota detectada y su probabilidad
-last_detected_note = None
-max_probability = 0.0
+# Variables globales para almacenar la nota con la mayor probabilidad
+highest_prob_note = None
+highest_probability = 0.0
 
 # Función para procesar las etiquetas detectadas
-def process_detected_labels(current_label, probability):
-    global last_detected_note, max_probability
+def process_detected_labels():
+    global highest_prob_note, highest_probability
 
-    # Elimina los últimos dos caracteres ' pr' de la etiqueta
-    cleaned_label = current_label.strip()[:-2]
-
-    # Verifica si la probabilidad supera el umbral y es mayor que la actual máxima probabilidad
-    if probability > 0.87 and probability > max_probability:
-        last_detected_note = cleaned_label
-        max_probability = probability
+    if highest_prob_note:
+        print(f"Nota detectada con mayor probabilidad: {highest_prob_note}")
+        message = {"note": highest_prob_note}  # 'Nota' como clave de los datos
+        client.publish(f"v1/devices/me/telemetry", json.dumps(message))
 
 # Espera por datos
 print("Esperando datos... Presiona Enter para salir.")
@@ -37,20 +53,27 @@ while True:
         # Busca coincidencias en la línea actual
         matches = pattern.findall(line)
 
-        # Procesa todas las coincidencias que superan el umbral
+        # Resetear valores para la nueva línea
+        highest_prob_note = None
+        highest_probability = 0.0
+
+        # Procesa todas las coincidencias
         for match in matches:
             label, value = match
             probability = float(value)
 
-            # Procesa la nota detectada
-            process_detected_labels(label, probability)
+            if probability > highest_probability:
+                highest_probability = probability
+                highest_prob_note = label
 
-        # Toma decisiones basadas en la nota detectada con la mayor probabilidad
-        #if last_detected_note is not None:
-         #   print(f"Nota detectada con mayor probabilidad: {last_detected_note}")
+        # Procesa la nota detectada con la mayor probabilidad
+        process_detected_labels()
 
     # Verifica si se ha presionado Enter para salir del bucle
     if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
         line = input()
         if line == '':
             break
+
+ser.close()
+client.loop_stop()
